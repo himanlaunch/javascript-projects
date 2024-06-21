7724e7f3666b8b9dcf9db54bf8bec098aa4da625
himanlaunch <himanshureddyk1995@gmail.com>
2024-06-20 23:18:25 -0500

    *.js with new features

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,32 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+console.log(language.slice(0,1)+language.slice(4,5));
 //2. Without using slice(), use method chaining to accomplish the same thing.
+let shortForm = language.substring(0, language.indexOf('S') + 1);
+console.log(shortForm); 
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+let initial = language[0] + language[4];
+console.log(`The abbreviation for '${language}' is '${initial}'.`); 
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+let initials = language.toUpperCase().slice(0, 1) + language.toLowerCase().slice(4, 5);
+console.log(`The abbreviation for '${language}' is '${initials}'.`);
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let words = notTitleCase.split(' ');
+
+// Capitalize the first letter of each word and lowercase the rest
+for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1).toLowerCase();
+}
+
+// Join the words back into a single string
+let titleCase = words.join(' ');
+
+console.log(titleCase);
--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -22,11 +22,18 @@ console.log(dna);
 let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
 
 //1) Replace the gene "GCT" with "AGG", and then print the altered strand.
-
+let alteredDNA = dnaTwo.replace("GCT", "AGG");
+console.log(alteredDNA);
 //2) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
-
+if (dnaTwo.indexOf("CAT") == -1) {
+    console.log("CAT gene found");
+  } else {
+    console.log("CAT gene NOT found");
+  }
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
-
+let fifthGene = dnaTwo.slice(15, 19);
+console.log(fifthGene);
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
-
+console.log(`The DNA strand is ${dnaTwo.length} characters long.`);
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+console.log(`${dna.slice(4,7).toLowerCase()}o ${dna.slice(dna.indexOf('CAT'),dna.indexOf('CAT')+3).toLowerCase()}`);

d1aface07a4b147e557d575a5b7a0920a97071a5
himanlaunch <himanshureddyk1995@gmail.com>
2024-07-15 20:39:13 -0500

    *.js with new features

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,32 @@
 
 let launchcode = {
-  
-}
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(number){}
+};
 
 module.exports = launchcode;
 
+launchcode.launchOutput = function(number) {
+    let output = '';
+  
+    if (number % 2 === 0) {
+      output += 'Launch';
+    }
+  
+    if (number % 3 === 0) {
+      output += 'Code';
+    }
+  
+    if (number % 5 === 0) {
+      output += ' Rocks';
+    }
+
+    if (output === '') {
+      output = "Rutabagas! That doesn't work";
+    }
+  
+    return output + '!';
+  };
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,47 @@
 // launchcode.test.js code:
+
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchOutput() method", function() {
 
-  // Write your unit tests here!
-  
+  test("Returns 'Launch!' when divisible by 2", function() {
+    const result = launchcode.launchOutput(2);
+    expect(result).toEqual('Launch!');
+  });
+
+  test("Returns 'Code!' when divisible by 3", function() {
+    const result = launchcode.launchOutput(3);
+    expect(result).toEqual('Code!');
+  });
+
+  test("Returns 'Rocks!' when divisible by 5", function() {
+    const result = launchcode.launchOutput(5);
+    expect(result).toEqual(' Rocks!');
+  });
+
+  test("Returns 'LaunchCode!' when divisible by 2 and 3", function() {
+    const result = launchcode.launchOutput(6);
+    expect(result).toEqual('LaunchCode!');
+  });
+
+  test("Returns 'Code Rocks!' when divisible by 3 and 5", function() {
+    const result = launchcode.launchOutput(15);
+    expect(result).toEqual('Code Rocks!');
+  });
+
+  test("Returns 'Launch Rocks!' when divisible by 2 and 5", function() {
+    const result = launchcode.launchOutput(10);
+    expect(result).toEqual('Launch Rocks!');
+  });
+
+  test("When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'", function() {
+    const result = launchcode.launchOutput(30);
+    expect(result).toEqual('LaunchCode Rocks!');
+  });
+
+  test("When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work!'", function() {
+    const result = launchcode.launchOutput(7);
+    expect(result).toEqual("Rutabagas! That doesn't work!");
+  });
+
 });
